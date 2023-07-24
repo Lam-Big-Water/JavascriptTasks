9ef8b569202e1da21ec1617784543ce139956b92
Sam <xlin67948@gmail.com>
2023-07-24 11:16:50 +0800

    2023/7/24

--- The_Modern_JS/JS_Fundamentals.js
+++ The_Modern_JS/JS_Fundamentals.js
@@ -339,6 +339,199 @@ if (account == 'Admin') {
   alert('Canceled');
 } else (alert('I don\'t know you'));
 ```
+
+---
+---
+
+while-for
+
+Last loop value
+importance: 3
+What is the last value alerted by this code? Why?
+
+```javascript
+let i = 3;
+
+while (i) {
+  alert( i-- );
+}
+```
+The answer: 3 2 1;
+
+
+Which values does the while loop show?
+importance: 4
+For every loop iteration, write down which value it outputs and then compare it with the solution.
+
+Both loops alert the same values, or not?
+
+```javascript
+The prefix form ++i:
+let i = 0;
+while (++i < 5) alert( i );
+The answer: 0 1 2 3 4 
+
+The postfix form i++
+let i = 0;
+while (i++ < 5) alert( i );
+The answer: 0 1 2 3 4 
+```
+The value returned by the increment is not used here, so there's no difference between i++ and ++i.
+
+
+Output even numbers in the loop
+importance: 5
+Use the for loop to output even numbers from 2 to 10.
+
+```javascript
+for(i = 2; i <= 10; i++) {
+  console.log(i);
+}
+let num = 2;
+while(num <= 10) {
+  console.log(num);
+  num++;
+}
+
+do {
+  console.log(num);
+  num++;
+} while (num <= 10);
+```
+
+Replace "for" with "while"
+importance: 5
+Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+```javascript
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+let i = 0;
+
+while (i < 3) {
+  alert(`number ${i}!`);
+  i++;
+}
+```
+
+
+Repeat until the input is correct
+importance: 5
+Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+
+```javascript
+let num;
+
+do {
+  num = prompt('Enter a number greater than 100 ?', 0);
+} while (num <= 100 && num);
+The check && num is false when num is null or an empty string. Then the while loop stops too.
+```
+
+
+Output prime numbers
+importance: 3
+An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+```javascript
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+  }
+  console.log(i);
+}
+```
+
+
+---
+---
+
+Switch
+
+Rewrite the "switch" into an "if"
+importance: 5
+Write the code using if..else which would correspond to the following switch:
+
+```javascript
+switch (browser) {
+  case 'Edge':
+    alert( "You've got the Edge!" );
+    break;
+
+  case 'Chrome':
+  case 'Firefox':
+  case 'Safari':
+  case 'Opera':
+    alert( 'Okay we support these browsers too' );
+    break;
+
+  default:
+    alert( 'We hope that this page looks ok!' );
+}
+
+
+let browser = 'Chrome';
+if (browser == 'Edge') {
+  alert('You\'ve got the Edge!');
+} else if (browser == 'Chrome'
+|| browser == 'Firefox'
+|| browser == 'Safari'
+|| browser == 'Opera') {
+  alert( 'Okay we support these browsers too' );
+} else (alert( 'We hope that this page looks ok!' ));
+```
+
+
+Rewrite "if" into "switch"
+importance: 4
+Rewrite the code below using a single switch statement:
+
+```javascript
+
+let a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+
+
+let a = +prompt('a?', '');
+alert(a);
+
+switch (a) {
+  case 0:
+   alert(0);
+   break;
+
+  case 1:
+   alert(1);
+   break;
+
+  case 2:
+  case 3:
+  alert('2,3');
+  break;
+
+  default:
+  alert('Wrong number!');
+}
+```
 */
 
 
