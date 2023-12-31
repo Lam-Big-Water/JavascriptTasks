3bb44275422fca2363211c037ec050ed6a0e9c8f
Sam <xlin67948@gmail.com>
2023-12-31 10:46:22 +0800

    object

--- The_Modern_JS/Tasks/JavaScript_Fundamentals/script.js
+++ The_Modern_JS/Tasks/JavaScript_Fundamentals/script.js
@@ -323,12 +323,96 @@ ask(
 );
 */
 
-const ask = (question, yes, no) => {
-  confirm(question) ? yes() : no()
+// const ask = (question, yes, no) => {
+//   confirm(question) ? yes() : no()
+// }
+
+// ask(
+//   "Do you agree?",
+//   function() { console.log("You agreed.") },
+//   function() { console.log("You canceled the execution.") }
+// )
+
+// * object
+/*
+Hello, object
+importance: 5
+Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object.
+*/
+
+const user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+console.log(user)
+
+
+/*
+Check for emptiness
+importance: 5
+Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false
+*/
+
+let schedule = {};
+
+console.log( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+console.log( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // if the loop has started, there is a property
+    return false;
+  }
+  return true;
 }
 
-ask(
-  "Do you agree?",
-  function() { console.log("You agreed.") },
-  function() { console.log("You canceled the execution.") }
-)
+/*
+Sum object properties
+importance: 5
+We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+If salaries is empty, then the result must be 0.
+*/
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+let sum = 0;
+
+for (key in salaries) {
+  console.log(salaries[key]);
+  sum += salaries[key];
+}
+console.log(sum)
+
